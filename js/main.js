8581e405ae3c9ad1d6dbcf7409705a9f3fbaf10d
Mohit Gupta <mohitgupta8888@gmail.com>
2018-12-08 16:25:42 +0530

    Updated

--- js/main.js
+++ js/main.js
@@ -16,9 +16,8 @@
 
 	///////////////////////////
 	// Smooth scroll
-	$("#nav .main-nav a[href^='#']").on('click', function(e) {
+	$("a[href^='#']").on('click', function(e) {
 		e.preventDefault();
-		var hash = this.hash;
 		$('html, body').animate({
 			scrollTop: $(this.hash).offset().top
 		}, 600);
